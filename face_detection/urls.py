94166b8eec38d6d40d977048dc7977cfb9ba9011
santosh_singh <sandy.singh21031988@gmail.com>
2019-07-23 21:34:30 +0530

    Setting layout and added bootstrap

--- face_detection/urls.py
+++ face_detection/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Custom urls here
     path('accounts/', include('accounts.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
